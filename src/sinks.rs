56a8fa94e524dfb61903a6d6ed7bcc266cad5bab
Kent Theo Fourie <42357713+GundamDweeb@users.noreply.github.com>
2023-11-11 01:26:29 +0200

    fix MAX u64 issue

--- src/sinks.rs
+++ src/sinks.rs
@@ -8,26 +8,24 @@ use substreams_entity_change::pb::entity::EntityChanges;
 use substreams_entity_change::tables::Tables;
 
 #[substreams::handlers::map]
-pub fn graph_out(
-    clock: Clock,
-    block: BalanceChanges,
-    token: StoreGetString,
-) -> Result<EntityChanges, Error> {
+pub fn graph_out(block: BalanceChanges, token: StoreGetString) -> Result<EntityChanges, Error> {
     let mut tables = Tables::new();
-    let block_num = clock.number.to_string();
-    let timestamp = clock.timestamp.unwrap().seconds.to_string();
 
     for storage_change in block.balance_changes {
         let token_lookup = token.get_last(&storage_change.contract);
         let token_found = token_lookup.is_some();
 
         if token_found {
-            let token = &get_erc20_token(storage_change.contract.clone()).unwrap();
-            tables
-                .create_row("Token", append_0x(&storage_change.contract))
-                .set("name", token.name.clone())
-                .set("decimals", token.decimals.clone())
-                .set("symbol", token.symbol.clone());
+            let token_option = &get_erc20_token(&storage_change.contract);
+            if token_option.is_some() {
+                let token = token_option.clone().unwrap();
+
+                tables
+                    .create_row("Token", append_0x(&storage_change.contract))
+                    .set("name", token.name.clone())
+                    .set("decimals", token.decimals.clone())
+                    .set("symbol", token.symbol.clone());
+            }
         }
 
         let id = format!("{}:{}", storage_change.contract, storage_change.owner);
@@ -46,7 +44,7 @@ pub fn graph_out(
             .set("owner", append_0x(&storage_change.owner))
             .set(
                 "balance",
-                BigInt::try_from(storage_change.new_balance).unwrap_or_default(),
+                BigInt::try_from(storage_change.new_balance).unwrap_or(BigInt::zero()),
             );
     }
 
